f9419640b2ecaaa2699b5582b68c7c729d7e0901
FL-gs <efge.crea@gmail.com>
2026-05-10 19:46:31 +0200

    bump to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.emuctrlr.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
