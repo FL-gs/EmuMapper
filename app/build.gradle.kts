7770ecaaeb25663b910408d115da7e80b914eade
FL-gs <efge.crea@gmail.com>
2026-05-17 16:53:08 +0200

    Bump to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.emumapper.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.6.0"
+        versionCode = 8
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
